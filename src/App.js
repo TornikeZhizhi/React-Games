b408b6a0184633e301e705213a0a41b4b6b2cd22
Ani-Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-01-26 23:53:15 +0400

    update img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Slot from "./pages/Slot";
+
+const router = createBrowserRouter([{ path: "/slot", element: <Slot /> }]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">headeდსდასr</header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Slot.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Slot.css";
+
+function Slot() {
+  return (
+    <div className="slot_container">
+      <div className="slot_row">
+        <div className="slot_item icon_k"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Slot;
