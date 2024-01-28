5f782250b18623c8455abdbcc67453382dc61253
Ani-Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-01-28 17:50:46 +0400

    update redirect file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Slot from "./pages/Slot";
+import Home from "./pages/Home/Home";
 
-const router = createBrowserRouter([{ path: "/slot", element: <Slot /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/slot", element: <Slot /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
