16b4f104c137b8f793431b52c3705712001e4e27
Ani-Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2024-07-23 03:04:09 +0400

    'update

--- src/pages/Piano/Piano.js
+++ src/pages/Piano/Piano.js
@@ -58,111 +58,140 @@ function Piano() {
       }
     };
 
+    const handleMouseDown = (event) => {
+      const key = event.target.textContent.toUpperCase();
+      const parentkey = event.target.parentElement.textContent.toUpperCase();
+      console.log(key, parentkey);
+      const note = keyToNote[key];
+      if (note) {
+        synth.triggerAttack(note);
+        event.target.classList.add("active");
+      }
+    };
+
+    const handleMouseUp = (event) => {
+      const key = event.target.textContent.toUpperCase();
+
+      const note = keyToNote[key];
+      if (note) {
+        synth.triggerRelease();
+        event.target.classList.remove("active");
+      }
+    };
+
     window.addEventListener("keydown", handleKeyDown);
     window.addEventListener("keyup", handleKeyUp);
+
+    const keys = document.querySelectorAll(".kb_parent");
+    keys.forEach((key) => {
+      key.addEventListener("mousedown", handleMouseDown);
+      key.addEventListener("mouseup", handleMouseUp);
+    });
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
       window.removeEventListener("keyup", handleKeyUp);
+      keys.forEach((key) => {
+        key.removeEventListener("mousedown", handleMouseDown);
+        key.removeEventListener("mouseup", handleMouseUp);
+      });
     };
   }, []);
 
   return (
-    <div className="piano_fluid">
-      <div id="p-wrapper">
-        <ul id="piano">
-          <li id="key-A">
-            <span className="keyboard_letter">A</span>
-            <div className="anchor"></div>
-          </li>
-          <li id="key-S">
-            <span className="keyboard_letter">S</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-Q">
-              <span className="keyboard_letter">Q</span>
-            </span>
-          </li>
-          <li id="key-D">
-            <span className="keyboard_letter">D</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-W">
-              <span className="keyboard_letter">W</span>
-            </span>
-          </li>
-          <li id="key-F">
-            <span className="keyboard_letter">F</span>
-            <div className="anchor"></div>
-          </li>
-          <li id="key-G">
-            <span className="keyboard_letter">G</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-E">
-              <span className="keyboard_letter">E</span>
-            </span>
-          </li>
-          <li id="key-H">
-            <span className="keyboard_letter">H</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-R">
-              <span className="keyboard_letter">R</span>
-            </span>
-          </li>
-          <li id="key-J">
-            <span className="keyboard_letter">J</span>
-            <div className="anchor"></div>
-          </li>
-          <li id="key-K">
-            <span className="keyboard_letter">K</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-T">
-              <span className="keyboard_letter">T</span>
-            </span>
-          </li>
-          <li id="key-L">
-            <span className="keyboard_letter">L</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-Y">
-              <span className="keyboard_letter">Y</span>
-            </span>
-          </li>
-          <li id="key-Z">
-            <span className="keyboard_letter">Z</span>
-            <div className="anchor"></div>
-          </li>
-          <li id="key-X">
-            <span className="keyboard_letter">X</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-U">
-              <span className="keyboard_letter">U</span>
-            </span>
-          </li>
-          <li id="key-C">
-            <span className="keyboard_letter">C</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-I">
-              <span className="keyboard_letter">I</span>
-            </span>
-          </li>
-          <li id="key-V">
-            <span className="keyboard_letter">V</span>
-            <div className="anchor"></div>
-          </li>
-          <li id="key-B">
-            <span className="keyboard_letter">B</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-O">
-              <span className="keyboard_letter">O</span>
-            </span>
-          </li>
-          <li id="key-N">
-            <span className="keyboard_letter">N</span>
-            <div className="anchor"></div>
-            <span className="piano_key" id="key-P">
-              <span className="keyboard_letter">P</span>
-            </span>
-          </li>
-        </ul>
-      </div>{" "}
+    <div id="p-wrapper">
+      <ul id="piano">
+        <li className="kb_parent" id="key-A">
+          <span className="keyboard_letter">A</span>
+          <div className="anchor"></div>
+        </li>
+        <li className="kb_parent" id="key-S">
+          <span className="keyboard_letter">S</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-Q">
+            <span className="keyboard_letter">Q</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-D">
+          <span className="keyboard_letter">D</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-W">
+            <span className="keyboard_letter">W</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-F">
+          <span className="keyboard_letter">F</span>
+          <div className="anchor"></div>
+        </li>
+        <li className="kb_parent" id="key-G">
+          <span className="keyboard_letter">G</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-E">
+            <span className="keyboard_letter">E</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-H">
+          <span className="keyboard_letter">H</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-R">
+            <span className="keyboard_letter">R</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-J">
+          <span className="keyboard_letter">J</span>
+          <div className="anchor"></div>
+        </li>
+        <li className="kb_parent" id="key-K">
+          <span className="keyboard_letter">K</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-T">
+            <span className="keyboard_letter">T</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-L">
+          <span className="keyboard_letter">L</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-Y">
+            <span className="keyboard_letter">Y</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-Z">
+          <span className="keyboard_letter">Z</span>
+          <div className="anchor"></div>
+        </li>
+        <li className="kb_parent" id="key-X">
+          <span className="keyboard_letter">X</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-U">
+            <span className="keyboard_letter">U</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-C">
+          <span className="keyboard_letter">C</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-I">
+            <span className="keyboard_letter">I</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-V">
+          <span className="keyboard_letter">V</span>
+          <div className="anchor"></div>
+        </li>
+        <li className="kb_parent" id="key-B">
+          <span className="keyboard_letter">B</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-O">
+            <span className="keyboard_letter">O</span>
+          </span>
+        </li>
+        <li className="kb_parent" id="key-N">
+          <span className="keyboard_letter">N</span>
+          <div className="anchor"></div>
+          <span className="piano_key" id="key-P">
+            <span className="keyboard_letter">P</span>
+          </span>
+        </li>
+      </ul>
     </div>
   );
 }
